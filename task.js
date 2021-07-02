a1bc50323f45812decfa8f06d2c1a90d40e39313
Konstantin-volley <83685869+Konstantin-volley@users.noreply.github.com>
2021-07-02 11:37:18 +0700

    Add files via upload

---
+++ task.js
@@ -0,0 +1,79 @@
+                                                                //Задание 1
+
+                                
+// let arr = [1,3,5,7,9,2,3]
+// function max(arr) {
+//     let max = arr[0]
+//     for(let i = 0; i < arr.length; i++) {
+//         if(arr[i] > max) {
+//             max = arr[i]
+//         }
+//     }
+//     return max
+// }
+
+// console.log(max(arr))
+
+
+// function min(arr) {
+//     let min = arr[0]
+//     for(let i = 0; i < arr.length; i++) {
+//         if(min > arr[i] ) {
+//             min = arr[i]
+//         }
+//     }
+//     return min
+// }
+// console.log(min(arr))
+
+
+                                                                //Задание 2
+
+
+// function arraySum(mass) {
+//         let a = 0
+//         for (let i = 0; i < arr.length; i++){
+//         a += mass[i]
+//     }
+//     console.log( a )
+//     return arraySum 
+// }
+// arraySum(arr)
+
+
+
+
+                                                                //Задание 3
+
+// for (let i = 1; i < arr.length; i++)
+// for ( let j = 0; j < i; j++)
+// if (arr[j] > arr[i]) {
+// let x = arr[i]
+// arr[i] = arr[j]
+// arr[j] = x
+// }
+// console.log( arr )
+
+
+// for (let i = 1; i < arr.length; i++)
+// for ( let j = 0; j < i; j++)
+// if (arr[j] < arr[i]) {
+// let x = arr[i]
+// arr[i] = arr[j]
+// arr[j] = x
+// }
+// console.log( arr )
+
+
+                                                                                                        //Оффтоп
+                                                                                                        // let arr = [1,3,5,7,9,2,3]
+                                                                                                        // // for (let i = 1; i < arr.length; i++)
+                                                                                                        // // for ( let j = 0; j < i; j++)
+                                                                                                        // // if (arr[j] < arr[i]) {
+                                                                                                        // //     let x = arr[i]
+                                                                                                        // //     arr[i] = arr[j]
+                                                                                                        // //     arr[j] = x
+                                                                                                        // // }
+                                                                                                        // // console.log( arr )
+
+                                                                                                        //почему не работает???
